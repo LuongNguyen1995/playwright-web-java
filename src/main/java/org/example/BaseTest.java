b22e0dc0e1d54090c7bad31d568f4c1ec5575a6a
Lương Nguyễn <luong.epu.dtvt1@gmail.com>
2023-09-25 08:44:16 +0700

    push 1 code

---
+++ src/main/java/org/example/BaseTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.*;
+
+public class BaseTest {
+    protected static Playwright playwright;
+    protected static Browser browser;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+
+}
